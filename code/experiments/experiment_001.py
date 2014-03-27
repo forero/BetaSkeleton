d231d16870b7c46bd32ce676e11e3a74af21779f
Jaime Forero <j.e.forero.romero@gmail.com>
2014-03-27 15:14:54 -0500

    no clean

--- code/experiments/experiment_001.py
+++ code/experiments/experiment_001.py
@@ -12,7 +12,5 @@ for i_mock in range(total_mocks):
             for i_mass, i_f_in in zip(base_mass, base_f_in):
 
                 command_all="make -f Makefile %s MOCK_ID=%02d BETA=%d CUT_MASS=%.1E FRAC=%.4f RSD=%d"%("all", i_mock, beta, i_mass, i_f_in, rsd)
-                command_clean="make -f Makefile %s MOCK_ID=%02d BETA=%d CUT_MASS=%.1E FRAC=%.4f RSD=%d"%("clean", i_mock, beta, i_mass, i_f_in, rsd)
 
-                retcode = call(command_clean,shell=True)
                 retcode = call(command_all,shell=True)
